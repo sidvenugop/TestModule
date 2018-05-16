7bac785359ec83543644f4d681e52a5b9fb09eed
sidvenugop <38498021+sidvenugop@users.noreply.github.com>
2018-05-16 20:47:01 +0530

    Create distelli.sh

---
+++ distelli.sh
@@ -0,0 +1,4 @@
+echo "DistelliAccessToken: '9EMWDPQZ9KVJ0E5242E54D9I3'" > /etc/distelli.yml
+echo "DistelliSecretKey: 'jzjjpiemik98mj9i5aubwvzj03oftx2gjz4cr'" >> /etc/distelli.yml
+echo "Environments:" >> /etc/distelli.yml
+echo "  - dev_lab" >> /etc/distelli.yml
