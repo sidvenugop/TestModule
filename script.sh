7902d3a38859f70a4645863fc5cf65edeedbe89c
sidvenugop <38498021+sidvenugop@users.noreply.github.com>
2018-05-16 20:17:57 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ else
 echo "DistelliAccessToken: '9EMWDPQZ9KVJ0E5242E54D9I3'" > /etc/distelli.yml
 echo "DistelliSecretKey: 'jzjjpiemik98mj9i5aubwvzj03oftx2gjz4cr'" >> /etc/distelli.yml
 echo "Environments:" >> /etc/distelli.yml
-echo "  - dev_lab" >> /etc/distelli.yml
+echo "  - Test" >> /etc/distelli.yml
 cd ~
 wget -qO- https://pipelines.puppet.com/download/client | sh
 /usr/local/bin/distelli agent install -conf /etc/distelli.yml
